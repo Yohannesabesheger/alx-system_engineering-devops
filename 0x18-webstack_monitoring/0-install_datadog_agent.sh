4d1a6c16cc6e2355c8098849ed96a35668edbc2f
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 <jonmersha@gmail.com>
2025-07-18 21:50:46 +0300

    KEY

--- 0x18-webstack_monitoring/0-install_datadog_agent.sh
+++ 0x18-webstack_monitoring/0-install_datadog_agent.sh
@@ -1,4 +1,2 @@
-
-DD_API_KEY=9c2ec6fa2617eff9f8669060926b9a58 \
-DD_SITE="us5.datadoghq.com" \
-bash -c "$(curl -L https://install.datadoghq.com/scripts/install_script_agent7.sh)"
+DD_API_KEY="9c2ec6fa2617eff9f8669060926b9a58"
+sudo DD_API_KEY=$DD_API_KEY bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
