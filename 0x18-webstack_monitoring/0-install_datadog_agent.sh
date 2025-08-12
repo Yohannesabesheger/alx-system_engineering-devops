a0568da5b9e67b3c93b22f9cd4a07aa901cd0f1c
Yohannes Mitike <113605459+Yohannesabesheger@users.noreply.github.com>
2025-08-12 16:11:31 +0300

    Update 0-install_datadog_agent.sh
    data Dog Configurations

--- 0x18-webstack_monitoring/0-install_datadog_agent.sh
+++ 0x18-webstack_monitoring/0-install_datadog_agent.sh
@@ -1,2 +1,3 @@
 DD_API_KEY="9c2ec6fa2617eff9f8669060926b9a58"
-sudo DD_API_KEY=$DD_API_KEY bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
+sudo DD_API_KEY=$DD_API_KEY DD_SITE="datadoghq.com" \
+bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
